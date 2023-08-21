e1f2448af86c8ad0a4271f42b6ff9d40eab5e06d
GeonHui2 <022686@naver.com>
2023-08-21 16:04:27 +0900

    :sparkles:: Kakao 로그인 기능 추가

---
+++ src/main/java/GDG/backend/global/oauth/kakao/KakaoMemberClient.java
@@ -0,0 +1,45 @@
+package GDG.backend.global.oauth.kakao;
+
+import GDG.backend.domain.oauth.domain.OauthMember;
+import GDG.backend.domain.oauth.client.OauthMemberClient;
+import GDG.backend.domain.oauth.domain.OauthServerType;
+import GDG.backend.global.oauth.kakao.client.KakaoApiClient;
+import GDG.backend.global.oauth.kakao.dto.KakaoMemberResponse;
+import GDG.backend.global.oauth.kakao.dto.KakaoToken;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class KakaoMemberClient implements OauthMemberClient {
+
+    private final KakaoApiClient kakaoApiClient;
+    private final KakaoOauthConfig kakaoOauthConfig;
+
+    @Override
+    public OauthServerType supportServer() {
+        return OauthServerType.KAKAO;
+    }
+
+    @Override
+    public OauthMember fetch(String authCode) {
+        KakaoToken tokenInfo = kakaoApiClient.fetchToken(tokenRequestParams(authCode)); // (1)
+        KakaoMemberResponse kakaoMemberResponse =
+                kakaoApiClient.fetchMember("Bearer " + tokenInfo.accessToken());  // (2)
+        return kakaoMemberResponse.toDomain();  // (3)
+    }
+
+    private MultiValueMap<String, String> tokenRequestParams(String authCode) {
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", kakaoOauthConfig.clientId());
+        params.add("redirect_uri", kakaoOauthConfig.redirectUri());
+        params.add("code", authCode);
+        params.add("client_secret", kakaoOauthConfig.clientSecret());
+        return params;
+    }
+}
