b4e9e0a623fab54c0d1c04113b6d23f4e18f0c9c
GeonHui2 <022686@naver.com>
2023-08-22 17:16:02 +0900

    :sparkles:: 회원가입 기능 추가

--- src/main/java/GDG/backend/domain/oauth/domain/repository/OauthMemberRepository.java
+++ src/main/java/GDG/backend/domain/oauth/domain/repository/OauthMemberRepository.java
@@ -2,6 +2,7 @@ package GDG.backend.domain.oauth.domain.repository;
 
 import GDG.backend.domain.oauth.domain.OauthId;
 import GDG.backend.domain.oauth.domain.OauthMember;
+import GDG.backend.domain.oauth.domain.OauthServerType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,4 +10,6 @@ import java.util.Optional;
 public interface OauthMemberRepository extends JpaRepository<OauthMember, Long> {
 
     Optional<OauthMember> findByOauthId(OauthId oauthId);
+
+    Optional<OauthMember> findByOauthId_OauthServerTypeAndEmail(OauthServerType oauthServerType, String email);
 }
