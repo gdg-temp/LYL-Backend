ab529083bc52053dceca9ab213de8297ca6e33b7
GeonHui2 <022686@naver.com>
2023-08-22 17:16:44 +0900

    :page_facing_up:: SecurityConfig 수정

--- src/main/java/GDG/backend/global/config/SecurityConfig.java
+++ src/main/java/GDG/backend/global/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package GDG.backend.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,6 +15,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable();
+        http.authorizeRequests()
+                .requestMatchers("/**").permitAll(); // 모든 경로에 대하여 인증을 요구하지 않습니다.
+
         return http.build();
     }
 }
