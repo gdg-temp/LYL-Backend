09b840bc21b2945348af9fe7e9d84e8bccce390e
GeonHui2 <022686@naver.com>
2023-09-01 20:40:21 +0900

    :sparkles:: Template CRUD 기능 추가

--- src/main/java/GDG/backend/domain/template/presentation/TemplateController.java
+++ src/main/java/GDG/backend/domain/template/presentation/TemplateController.java
@@ -1,11 +1,14 @@
 package GDG.backend.domain.template.presentation;
 
 import GDG.backend.domain.template.presentation.dto.request.AddTemplateRequest;
+import GDG.backend.domain.template.presentation.dto.request.UpdateTemplateRequest;
 import GDG.backend.domain.template.presentation.dto.response.TemplateProfileResponse;
 import GDG.backend.domain.template.service.TemplateService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/template")
@@ -14,8 +17,28 @@ public class TemplateController {
 
     private final TemplateService templateService;
 
-    @PostMapping("/{cardId}")
-    public TemplateProfileResponse addTemplate(@PathVariable Long cardId, @RequestBody @Valid AddTemplateRequest addTemplateRequest) {
-        return templateService.addTemplate(cardId, addTemplateRequest);
+    @PostMapping()
+    public TemplateProfileResponse addTemplate(@RequestBody @Valid AddTemplateRequest addTemplateRequest) {
+        return templateService.addTemplate(addTemplateRequest);
+    }
+
+    @PatchMapping("/{templateId}")
+    public TemplateProfileResponse updateTemplate(@PathVariable Long templateId, @RequestBody @Valid UpdateTemplateRequest updateTemplateRequest) {
+        return templateService.updateTemplate(templateId, updateTemplateRequest);
+    }
+
+    @DeleteMapping("/{templateId}")
+    public void deleteTemplate(@PathVariable Long templateId) {
+        templateService.deleteTemplate(templateId);
+    }
+
+    @GetMapping("/{templateId}")
+    public TemplateProfileResponse getTemplateProfile(@PathVariable Long templateId) {
+        return templateService.getTemplateProfile(templateId);
+    }
+
+    @GetMapping
+    public List<TemplateProfileResponse> getTemplateList() {
+        return templateService.getTemplateList();
     }
 }
