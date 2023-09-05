c575dd9a256818b6d01e19d4fad0daf80bc9ffc5
GeonHui2 <022686@naver.com>
2023-09-05 14:16:39 +0900

    :sparkles:: RefreshToken 저장 로직 추가

--- src/main/java/GDG/backend/domain/user/domain/RefreshToken.java
+++ src/main/java/GDG/backend/domain/user/domain/RefreshToken.java
@@ -1,17 +1,18 @@
 package GDG.backend.domain.user.domain;
 
 import GDG.backend.global.database.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
 @Entity
@@ -22,11 +23,25 @@ import static lombok.AccessLevel.PROTECTED;
 @EntityListeners(AuditingEntityListener.class)
 public class RefreshToken extends BaseEntity {
 
-    @Id
+    @Id @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "refresh_token_id")
+    private Long id;
+
     private String refreshToken;
 
+    @CreatedDate
+    @Column(nullable = false)
+    LocalDateTime createdAt;
+
+    Long userId;
+
     @Builder
-    public RefreshToken(String refreshToken) {
+    public RefreshToken(String refreshToken, Long userId) {
+        this.refreshToken = refreshToken;
+        this.userId = userId;
+    }
+
+    public void updateRefreshToken(String refreshToken) {
         this.refreshToken = refreshToken;
     }
 }
