c173fa55e5102763081a9e8c1ffe647c203e6db6
GeonHui2 <022686@naver.com>
2023-09-23 23:14:10 +0900

    :recycle:: Card 리팩토링

--- src/main/java/GDG/backend/domain/businesscard/presentation/dto/request/CreateBusinessCardRequest.java
+++ src/main/java/GDG/backend/domain/businesscard/presentation/dto/request/CreateBusinessCardRequest.java
@@ -1,20 +1,25 @@
 package GDG.backend.domain.businesscard.presentation.dto.request;
 
 import GDG.backend.domain.businesscard.domain.WorkType;
+import GDG.backend.domain.link.domain.Link;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.annotation.Nullable;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public record CreateBusinessCardRequest(
         String name,
+        @Schema(description = "프로필 이미지 URL")
+        String profileImage,
         String email,
-        WorkType workType,
-        String job,
-        String position,
+        String introduction,
+        String mbti,
+        @Schema(description = "템플릿 URL")
+        String template,
         String companyName,
-        String companyAddress,
-        LocalDate birth,
-        @Schema(description = "템플릿 이미지 URL")
-        String templateURL
+        String position,
+        @Nullable
+        List<Link> links
 ) {
 }
--- src/main/java/GDG/backend/domain/businesscard/service/BusinessCardServiceUtils.java
+++ src/main/java/GDG/backend/domain/businesscard/service/BusinessCardServiceUtils.java
@@ -1,12 +1,15 @@
 package GDG.backend.domain.businesscard.service;
 
 import GDG.backend.domain.businesscard.domain.BusinessCard;
+import GDG.backend.domain.businesscard.presentation.dto.response.BusinessCardProfileResponse;
 import GDG.backend.domain.businesscard.presentation.dto.response.MyBusinessCardListResponse;
 import GDG.backend.domain.user.domain.User;
 
+import java.util.List;
+
 public interface BusinessCardServiceUtils {
 
-    MyBusinessCardListResponse getMyBusinessCardListResponse(User user);
+    List<BusinessCardProfileResponse> getMyBusinessCardListResponse();
     BusinessCard queryBusinessCard(Long businessCardId);
 
     BusinessCard validHost(Long cardId);
