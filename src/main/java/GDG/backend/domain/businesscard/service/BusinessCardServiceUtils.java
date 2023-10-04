05fbc6062dc752afccb7685fb15b0344c9b388ea
GeonHui2 <022686@naver.com>
2023-10-04 23:16:09 +0900

    :bug:: 버그 수정

--- src/main/java/GDG/backend/domain/businesscard/service/BusinessCardServiceUtils.java
+++ src/main/java/GDG/backend/domain/businesscard/service/BusinessCardServiceUtils.java
@@ -2,9 +2,6 @@ package GDG.backend.domain.businesscard.service;
 
 import GDG.backend.domain.businesscard.domain.BusinessCard;
 import GDG.backend.domain.businesscard.presentation.dto.response.BusinessCardProfileResponse;
-import GDG.backend.domain.businesscard.presentation.dto.response.MyBusinessCardListResponse;
-import GDG.backend.domain.user.domain.User;
-
 import java.util.List;
 
 public interface BusinessCardServiceUtils {
