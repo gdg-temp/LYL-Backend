65249af48d9a618111190848849532e8aa5714a5
GeonHui2 <022686@naver.com>
2023-11-25 16:10:13 +0900

    :bug:: security 수정

--- src/main/java/GDG/backend/global/config/WebConfig.java
+++ src/main/java/GDG/backend/global/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://linkyour.link")
+                .allowedOrigins("https://client-lovat-eta.vercel.app")
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.POST.name(),
